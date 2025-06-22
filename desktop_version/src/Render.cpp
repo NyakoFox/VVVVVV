3ab8e4eeb69bdcc5372f4472436a71310ee9d68c
mothbeanie <shinywolf07ontheinternets@gmail.com>
2025-06-21 20:42:59 -0700

    I FUCKED UP
    okay.

--- desktop_version/src/Render.cpp
+++ desktop_version/src/Render.cpp
@@ -3523,7 +3523,7 @@ void shoprender(void)
                     {
                         font::print_wrap(PR_LEFT, 16, desc_y + 16 + 8 * 6, ("Largest spotted: " + help.String(info.largest) + "cm").c_str(), 196, 196, 196);
                     }
-                    if (info.smallest >= ((FishItem*)item)->min_size)
+                    if (info.smallest <= ((FishItem*)item)->min_size)
                     {
                         font::print_wrap(PR_LEFT, 16, desc_y + 16 + 8 * 7, ("Smallest spotted: " + help.String(info.smallest) + "cm \u2605").c_str(), 255 - help.glow, 255 - help.glow, 196);
                     }
